16fd4dc98c5ab26c69257166e5e3de76b5af2289
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2020-11-10 22:43:00 +0900

    投稿機能、詳細機能、編集機能を実装

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,4 +1,47 @@
 class TargetsController < ApplicationController
   def index
+    @users = User.all
+  end
+  
+  def show
+    @users = User.all
+    @user = User.find(params[:id])
+    @targets = @user.targets.order('target_date DESC')
+  end
+
+  def new
+    @user = User.find(current_user.id)
+    @targets = @user.targets.order('target_date DESC')
+    @target = Target.new
+  end
+  
+  def create
+    @target = Target.create(target_params)
+    if @target.save
+      redirect_to action: :new
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @user = User.find(current_user.id)
+    @targets = @user.targets.order('target_date DESC')
+    @target = Target.find(params[:id])
+  end
+  
+  def update
+    @target = Target.find(params[:id])
+    if @target.update(target_params)
+      redirect_to action: :new
+    else
+      render :edit
+    end
+  end
+
+  private
+  def target_params
+    
+    params.require(:target).permit(:target_date, :content).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'targets/index'
   root to: "targets#index"
-  resources :targets, only: [:new, :create]
+  resources :targets, only: [:new, :create, :show, :edit, :update, :destroy]
 end
