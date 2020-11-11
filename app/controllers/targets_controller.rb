98c5bef201bfe38d74defd64f5a30750c52bf22c
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2020-11-11 11:21:30 +0900

    未ログイン時の遷移と重複する記述をまとめた

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,17 +1,19 @@
 class TargetsController < ApplicationController
+  before_action :set_user_where, only: [:index, :show]
+  before_action :set_current_user, only: [:new, :edit]
+  before_action :set_targets, only: [:new, :edit]
+  before_action :set_target_find_id, only: [:edit, :update, :destroy, :move_to_index]
+  before_action :move_to_index, only: [:edit, :destroy]
+
   def index
-    @users = User.all
   end
   
   def show
-    @users = User.all
     @user = User.find(params[:id])
-    @targets = @user.targets.order('target_date DESC')
+    set_targets
   end
-
+  
   def new
-    @user = User.find(current_user.id)
-    @targets = @user.targets.order('target_date DESC')
     @target = Target.new
   end
   
@@ -25,13 +27,9 @@ class TargetsController < ApplicationController
   end
   
   def edit
-    @user = User.find(current_user.id)
-    @targets = @user.targets.order('target_date DESC')
-    @target = Target.find(params[:id])
   end
   
   def update
-    @target = Target.find(params[:id])
     if @target.update(target_params)
       redirect_to action: :new
     else
@@ -40,14 +38,34 @@ class TargetsController < ApplicationController
   end
   
   def destroy
-    @target = Target.find(params[:id])
     @target.destroy
     redirect_to action: :new
+  end
+  
+  def move_to_index
+    unless current_user.id == @target.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def set_user_where
+    @users = User.where(private_mode: 0)
+  end
+  
+  def set_current_user
+    @user = User.find(current_user.id)
+  end
+  
+  def set_targets
+    @targets = @user.targets.order('target_date DESC')
+  end
+  
+  def set_target_find_id
+    @target = Target.find(params[:id])
   end
 
   private
   def target_params
-    
     params.require(:target).permit(:target_date, :content).merge(user_id: current_user.id)
   end
 end
